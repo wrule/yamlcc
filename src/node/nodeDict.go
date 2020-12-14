c66d8d3dbcae5583d2797481b4cf103ea05b052d
jimao <1982775886@qq.com>
2020-12-14 21:03:02 +0800

    nodeDict构造

--- src/node/nodeDict.go
+++ src/node/nodeDict.go
@@ -9,6 +9,11 @@ type Dict struct {
 	*Com
 }
 
+// Nodes 获取子节点列表
+func (me *Dict) Nodes() []INode {
+	return me.nodes
+}
+
 // Type 类型
 func (me *Dict) Type() ENodeType {
 	return NodeTypeDict
@@ -19,17 +24,29 @@ func (me *Dict) BeginningOf(text string) (string, string) {
 	return "", ""
 }
 
+// getNodes 获取节点
+func getNodes(
+	srcMap map[interface{}]interface{},
+) []INode {
+	rst := []INode{}
+	for key, value := range srcMap {
+		node := BuildNode(key)
+		leafNode := BuildLeafNode(value)
+		Link(node, leafNode)
+		rst = append(rst, node)
+	}
+	return rst
+}
+
 // NewDict 构造函数
 func NewDict(value map[interface{}]interface{}) *Dict {
-	nodes := []INode{}
-	for _key, _value := range value {
-		node := BuildNode(_key)
-		leafNode := BuildLeafNode(_value)
-		Link(node, leafNode)
-		nodes = append(nodes, node)
-	}
-	return &Dict{
+	dict := &Dict{
 		value: value,
-		nodes: nodes,
 	}
+	nodes := getNodes(dict.value)
+	for _, node := range nodes {
+		node.SetPrev(dict)
+	}
+	dict.nodes = nodes
+	return dict
 }
