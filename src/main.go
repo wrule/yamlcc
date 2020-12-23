6438855e40803ca758cf82f0ee269a15df3533ea
jimao <1982775886@qq.com>
2020-12-23 13:45:11 +0800

    测试代码

--- src/main.go
+++ src/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"./node"
+	"gopkg.in/yaml.v2"
+)
+
+func readBytes(filePath string) []byte {
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic("读取语法定义文件出错")
+	}
+	return bytes
+}
+
+func readString(filePath string) string {
+	return string(readBytes(filePath))
+}
 
 func main() {
-	fmt.Println("你好，世界，测试提交")
+	bytes := readBytes("/home/gu/桌面/yamlcc/lang/lua/lua.yaml")
+	m := make(map[interface{}]interface{})
+	yaml.Unmarshal(bytes, &m)
+	dict := node.BuildNodes(m)
+	fmt.Println(dict)
+	// code := readString("test/lua/1.lua")
+	// matchingText, nextText, success := dict.BeginningTrimOf(code)
+	// fmt.Println("匹配到:", matchingText)
+	// fmt.Println("剩下的:", nextText)
+	// fmt.Println("结果:", success)
+	// fmt.Println("你好")
 }
