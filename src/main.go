974720fa5a12b226cfda0078754d21fec2cc6aa1
jimao <1982775886@qq.com>
2020-12-29 17:06:33 +0800

    开始测试

--- src/main.go
+++ src/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"sort"
 
+	"./node"
 	"gopkg.in/yaml.v2"
 )
 
@@ -21,24 +21,10 @@ func readString(filePath string) string {
 }
 
 func main() {
-	bytes := readBytes("/home/gu/桌面/yamlcc/lang/lua/lua.yaml")
+	bytes := readBytes("/home/gu/桌面/yamlcc/lang/lua/luanew.yaml")
 	m := make(map[interface{}]interface{})
 	yaml.Unmarshal(bytes, &m)
-	nums := []int{3, 2, 1, 5, 4}
-	sort.Slice(nums, func(a, b int) bool {
-		return nums[a] > nums[b]
-	})
-	fmt.Println(nums)
-	// root := node.Compile(m)
-	// fmt.Println(root.BeginningTrimOf(` 	你好，世界`))
-	// fmt.Println(root.Nexts()[0].Nexts())
-	// for _, node := range nodes {
-	// 	node.Print()
-	// }
-	// code := readString("test/lua/1.lua")
-	// matchingText, nextText, success := dict.BeginningTrimOf(code)
-	// fmt.Println("匹配到:", matchingText)
-	// fmt.Println("剩下的:", nextText)
-	// fmt.Println("结果:", success)
-	// fmt.Println("你好")
+	root := node.Compile(m)
+	rst := root.BeginningTrimOf(`  1234`)
+	fmt.Printf("match: %s\nnext: %s\nsuccess: %v\n", rst.Match(), rst.Next(), rst.Success())
 }
