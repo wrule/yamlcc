06648b8dad4bfb6ab7e5f94dc34c0130a8611d64
jimao <1982775886@qq.com>
2021-01-02 00:17:12 +0800

    com的BeginningOfX既定完成

--- src/node/comParse.go
+++ src/node/comParse.go
@@ -7,7 +7,22 @@ func (me *Com) BeginningOf(text string) *Rst {
 
 // BeginningOfX s
 func (me *Com) BeginningOfX(text string, trimHead bool) *Rst {
-	return nil
+	ivdRst := NewRst("", text, true)
+	// 按需进行头部修整匹配
+	if trimHead {
+		ivdRst = me.Me().GetDef("invalid").BeginningOfX(text, false)
+	}
+	// 进行节点的原生匹配
+	meRst := me.Me().BeginningOf(ivdRst.Next())
+	if meRst.Success() {
+		// 进行下节点的下推匹配（头部修整）
+		nextRst := me.Me().NextsBeginningOfX(meRst.Next(), true)
+		if nextRst.Success() {
+			return NewRst(ivdRst.Match()+meRst.Match()+nextRst.Match(), nextRst.Next(), true)
+		}
+		return NewRst(ivdRst.Match()+meRst.Match()+nextRst.Match(), nextRst.Next(), false)
+	}
+	return NewRst(ivdRst.Match()+meRst.Match(), meRst.Next(), false)
 }
 
 // NextsBeginningOfX s
