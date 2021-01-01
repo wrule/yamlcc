996c334ae452560fc2286946956cbeb2dff078d7
jimao <1982775886@qq.com>
2021-01-02 02:02:20 +0800

    NextsBeginningOfX逻辑初步写好

--- src/node/comParse.go
+++ src/node/comParse.go
@@ -1,5 +1,7 @@
 package node
 
+import "sort"
+
 // BeginningOf s
 func (me *Com) BeginningOf(text string) *Rst {
 	return me.Me().BeginningOf(text)
@@ -27,14 +29,17 @@ func (me *Com) BeginningOfX(text string, trimHead bool) *Rst {
 
 // NextsBeginningOfX s
 func (me *Com) NextsBeginningOfX(text string, trimHead bool) *Rst {
-	successList := []*Rst{}
-	failureList := []*Rst{}
+	// 方便逻辑直接取用
+	ivdRst := me.invalidMatch(text, trimHead)
 
 	// 非逻辑检查
 	if me.NotsCheck(text, trimHead) == false {
-		ivdRst := me.invalidMatch(text, trimHead)
 		return NewRst(ivdRst.Match(), ivdRst.Next(), false)
 	}
+
+	// 定义成功失败结果列
+	successList := []*Rst{}
+	failureList := []*Rst{}
 
 	// 遍历下推匹配逻辑节点，并且采集成功失败结果
 	for _, log := range me.Me().NextLogs() {
@@ -45,7 +50,34 @@ func (me *Com) NextsBeginningOfX(text string, trimHead bool) *Rst {
 			failureList = append(failureList, rst)
 		}
 	}
-	return nil
+
+	// 其他命令逻辑
+	if len(successList) < 1 && me.Me().NextOther() != nil {
+		if rst := me.Me().NextOther().BeginningOfX(text, trimHead); rst.Success() {
+			successList = append(successList, rst)
+		} else {
+			failureList = append(failureList, rst)
+		}
+	}
+
+	// 按匹配长度排序（贪心）（这里有可能存在歧义要处理哦）
+	sort.Slice(successList, func(a, b int) bool {
+		return len(successList[a].Match()) > len(successList[b].Match())
+	})
+	sort.Slice(failureList, func(a, b int) bool {
+		return len(failureList[a].Match()) > len(failureList[b].Match())
+	})
+
+	// 拼接结果
+	rstList := []*Rst{}
+	rstList = append(rstList, successList...)
+	rstList = append(rstList, failureList...)
+
+	// 返回结果
+	if len(rstList) > 0 {
+		return rstList[0]
+	}
+	return NewRst(ivdRst.Match(), ivdRst.Next(), false)
 }
 
 func (me *Com) invalidMatch(text string, trimHead bool) *Rst {
