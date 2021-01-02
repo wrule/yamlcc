ffe82605b26124655e77651f38b12b16450ecd08
jimao <1982775886@qq.com>
2021-01-03 00:40:11 +0800

    back

--- src/back.go
+++ src/back.go
@@ -1,19 +1,28 @@
 package main
 
+import "fmt"
+
 type Back struct {
+	hops int
 	Com
+}
+
+func (me *Back) Hops() int {
+	return me.hops
 }
 
 func (me *Back) BeginningOf(text string) *Rst {
 	return NewRst("", text, true)
 }
 
-func NewBack() *Back {
-	rst := &Back{}
-	rst.Com = NewCom(rst, nil)
+func NewBack(num int) *Back {
+	rst := &Back{
+		hops: num,
+	}
+	rst.Com = NewCom(rst, num)
 	return rst
 }
 
 func (me *Back) Print() {
-
+	fmt.Printf("回跳节点: %v\n", me.Hops())
 }
