33ae8945f363c9a7252e95488d8e100be94e94e3
jimao <1982775886@qq.com>
2021-01-03 21:35:05 +0800

    root的注释

--- src/root.go
+++ src/root.go
@@ -2,20 +2,24 @@ package main
 
 import "fmt"
 
+// Root 根节点
 type Root struct {
 	Com
 }
 
+// BeginningOf s
 func (me *Root) BeginningOf(text string) *Rst {
 	return NewRst("", text, true)
 }
 
+// NewRoot 构造函数
 func NewRoot() *Root {
 	rst := &Root{}
 	rst.Com = NewCom(rst, ".root")
 	return rst
 }
 
+// Print s
 func (me *Root) Print() {
 	fmt.Printf("根节点\n")
 }
