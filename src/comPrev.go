603db156ecd519359511a92c47ad427e4cd04a83
jimao <1982775886@qq.com>
2021-01-04 14:10:49 +0800

    注释

--- src/comPrev.go
+++ src/comPrev.go
@@ -1,9 +1,11 @@
 package main
 
+// Prev 获取上节点
 func (me *Com) Prev() INode {
 	return me.prev
 }
 
+// PrevN 获取上n个节点
 func (me *Com) PrevN(n int) INode {
 	rst := me.Me()
 	for i := 0; i < n && rst != nil; i++ {
@@ -15,6 +17,7 @@ func (me *Com) PrevN(n int) INode {
 	return rst
 }
 
+// SetPrev 设置上节点
 func (me *Com) SetPrev(prev INode) {
 	me.prev = prev
 }
