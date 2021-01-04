49e7d54dc12b0eb81d480326b5638496eac74e29
jimao <1982775886@qq.com>
2021-01-04 11:14:04 +0800

    end

--- src/end.go
+++ src/end.go
@@ -1,19 +1,25 @@
 package main
 
+import "fmt"
+
+// End 结束命令节点
 type End struct {
 	Com
 }
 
+// BeginningOf s
 func (me *End) BeginningOf(text string) *Rst {
 	return NewRst("", text, true)
 }
 
+// NewEnd 构造函数
 func NewEnd() *End {
 	rst := &End{}
-	rst.Com = NewCom(rst, nil)
+	rst.Com = NewCom(rst, ".end")
 	return rst
 }
 
+// Print s
 func (me *End) Print() {
-
+	fmt.Printf("结束命令节点\n")
 }
