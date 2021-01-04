7338e3cb40df5b4748392a8442fa14b96b8f81b4
jimao <1982775886@qq.com>
2021-01-04 11:00:35 +0800

    not other

--- src/not.go
+++ src/not.go
@@ -1,19 +1,25 @@
 package main
 
+import "fmt"
+
+// Not 非命令节点
 type Not struct {
 	Com
 }
 
+// BeginningOf s
 func (me *Not) BeginningOf(text string) *Rst {
 	return NewRst("", text, true)
 }
 
+// NewNot 构造函数
 func NewNot() *Not {
 	rst := &Not{}
-	rst.Com = NewCom(rst, nil)
+	rst.Com = NewCom(rst, ".not")
 	return rst
 }
 
+// Print s
 func (me *Not) Print() {
-
+	fmt.Printf("非命令节点\n")
 }
--- src/other.go
+++ src/other.go
@@ -2,17 +2,20 @@ package main
 
 import "fmt"
 
+// Other 其他命令节点
 type Other struct {
 	Com
 }
 
+// BeginningOf s
 func (me *Other) BeginningOf(text string) *Rst {
 	return NewRst("", text, true)
 }
 
+// NewOther 构造函数
 func NewOther() *Other {
 	rst := &Other{}
-	rst.Com = NewCom(rst, nil)
+	rst.Com = NewCom(rst, ".other")
 	return rst
 }
 
