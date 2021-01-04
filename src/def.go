a60fd5b76ddc9e5d5d03235189bee02259bc6c79
jimao <1982775886@qq.com>
2021-01-04 11:35:39 +0800

    def

--- src/def.go
+++ src/def.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
+// Def 定义节点
 type Def struct {
 	defName string
 	Com
 }
 
+// DefName 定义名称
 func (me *Def) DefName() string {
 	return me.defName
 }
 
+// BeginningOf s
 func (me *Def) BeginningOf(text string) *Rst {
 	return me.NextsBeginningOfX(text)
 }
 
+// NewDef 构造函数
 func NewDef(text string) *Def {
 	rst := &Def{
 		defName: text[1:],
@@ -23,6 +27,7 @@ func NewDef(text string) *Def {
 	return rst
 }
 
+// Print s
 func (me *Def) Print() {
 	fmt.Printf("定义节点: %v\n", me.DefName())
 }
